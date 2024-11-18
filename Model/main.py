04a340f9572439a0bf7130d0d4f2eede0bb3b826
DafterPlay <dafterplay@mail.ru>
2024-11-18 16:31:31 +0300

    add model step one

--- Model/main.py
+++ Model/main.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 
 def main():
     model = Model(1)
-    for _ in range(1000):
-        model.update(50)
+    model.P = 0
+    for _ in range(2000):
+        model.update(100)
     plt.plot(model.t_hist, model.P_hist)
     plt.show()
     plt.plot(model.t_hist, model.hz_hist)
--- Model/model.py
+++ Model/model.py
@@ -3,30 +3,24 @@ from math import tan
 
 class Model:
     def __init__(self, dt):
-        self.dt = dt  # Размер шага в модели
+        self.dt = dt  # Размер шага в модели в секундах
         self.t = 0  # Текущее время в модели
         self.P = 0  # Текущее давление в модели
         self.hz_hist = [0]
         self.P_hist = [0]
         self.t_hist = [0]
 
-    def update(self, hz, is_drain=True, is_engine=True):
-        self.P -= self.drain(is_drain)
-        self.P += self.engine(hz, is_engine)
-        self.P = min(max(self.P, 0), 100)
+    def update(self, percent, is_drain=True, is_engine=True):
+        for _ in range(self.dt):
+            self.P += self.drain(is_drain) + self.engine(percent, is_engine)
+            self.P = min(max(self.P, 0), 40)
         self.t += self.dt
         self.P_hist.append(self.P)
-        self.hz_hist.append(hz)
+        self.hz_hist.append(percent)
         self.t_hist.append(self.t)
 
     def drain(self, is_drain):
-        return (self.P * 0.0025 + 0.01) * self.dt if is_drain else 0
+        return 0.09556781 / (self.P + 4.19694606) - 0.02272167 if is_drain else 0
 
-    def engine(self, hz, is_engine):
-        return tan(hz / 57.3) * self.dt * 0.1 if is_engine else 0
-
-
-"""
-TODO:
-1. Снять данные для 20, 30, 40 Гц и посмотреть на динамику для drain и engine.
-"""
+    def engine(self, percent, is_engine):
+        return (0.02865982 * percent + 0.00110959) / 60 if is_engine else 0
