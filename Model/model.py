0607e4bafff6545063d24674ba28ce8db42e14a3
DafterPlay <dafterplay@mail.ru>
2024-11-25 12:56:15 +0300

    add noise

--- Model/model.py
+++ Model/model.py
@@ -11,7 +11,7 @@ class Model:
         for _ in range(self.dt):
             self.P += self.drain(is_drain) + self.engine(percent, is_engine)
             self.P = max(self.P, 0)
-        self.P_noise = self.P #+ random() * 0.01 - 0.005
+        self.P_noise = self.P + random() * 0.01 - 0.005
     
     def drain(self, is_drain):
         return 0.09556781 / (self.P + 4.19694606) - 0.02272167 if is_drain else 0
